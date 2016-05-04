7db4a4e94cc1cae860aeb00180d377f810f2c769
big_data_jxz1 <13882261457@139.com>
2016-05-05 07:16:56 +0800

    Update 逻辑斯特回归_建模剖析1.scala

--- mllib_analysis/regression/逻辑斯特回归_建模剖析1.scala
+++ mllib_analysis/regression/逻辑斯特回归_建模剖析1.scala
@@ -1,4 +1,4 @@
-//线性回归_建模剖析1
+//逻辑斯特回归_建模剖析1
 
 //一、数学理论
 
@@ -7,8 +7,6 @@
 package org.apache.spark.mllib_analysis.regression
 
 import org.apache.spark.{SparkConf,SparkContext}
- 
-import org.apache.spark.SparkContext
 import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionModel}
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.regression.LabeledPoint
@@ -46,15 +44,13 @@ println("Precision = " + precision)
 //保存和加载模型
 model.save(sc, "myModelPath")
 val sameModel = LogisticRegressionModel.load(sc, "myModelPath")
+
 }
 
 //三、源码调用解析
-3.1 训练模型
-
-  如上所述，在MLlib中，分别使用了梯度下降法和L-BFGS实现逻辑回归参数的计算。这两个算法的实现我们会在最优化章节介绍，这里我们介绍公共的部分。
-
-  LogisticRegressionWithLBFGS和LogisticRegressionWithSGD的入口函数均是GeneralizedLinearAlgorithm.run，下面详细分析该方法。
-
+//1、训练模型
+//逻辑斯特回归中，分别使用了梯度下降法和L-BFGS实现逻辑回归参数的计算。这两个算法的实现在最优化部分进行介绍，这里介绍公共的部分。
+//LogisticRegressionWithLBFGS和LogisticRegressionWithSGD的入口函数均是GeneralizedLinearAlgorithm.run，下面详细分析该方法。
 def run(input: RDD[LabeledPoint]): M = {
     if (numFeatures < 0) {
       //计算特征数
@@ -71,10 +67,9 @@ def run(input: RDD[LabeledPoint]): M = {
     }
     run(input, initialWeights)
 }
-  上面的代码初始化权重向量，向量的值均初始化为0。需要注意的是，addIntercept表示是否添加截距(Intercept，指函数图形与坐标的交点到原点的距离)，默认是不添加的。numOfLinearPredictor表示二元逻辑回归模型的个数。 我们重点看run(input, initialWeights)的实现。它的实现分四步。
+//上面的代码初始化权重向量，向量的值均初始化为0。需要注意的是，addIntercept表示是否添加截距(Intercept，指函数图形与坐标的交点到原点的距离)，默认是不添加的。numOfLinearPredictor表示二元逻辑回归模型的个数。 我们重点看run(input, initialWeights)的实现。它的实现分四步。
 
-3.1.1 根据提供的参数缩放特征并添加截距
-
+//3.1、根据提供的参数缩放特征并添加截距
 val scaler = if (useFeatureScaling) {
       new StandardScaler(withStd = true, withMean = false).fit(input.map(_.features))
     } else {
@@ -100,7 +95,7 @@ val initialWeightsWithIntercept = if (addIntercept && numOfLinearPredictor == 1)
       /** If `numOfLinearPredictor > 1`, initialWeights already contains intercepts. */
       initialWeights
     }
-  在最优化过程中，收敛速度依赖于训练数据集的条件数(condition number)，缩放变量经常可以启发式地减少这些条件数，提高收敛速度。不减少条件数，一些混合有不同范围列的数据集可能不能收敛。 在这里使用StandardScaler将数据集的特征进行缩放。详细信息请看StandardScaler。appendBias方法很简单，就是在每个向量后面加一个值为1的项。
+//在最优化过程中，收敛速度依赖于训练数据集的条件数(condition number)，缩放变量经常可以启发式地减少这些条件数，提高收敛速度。不减少条件数，一些混合有不同范围列的数据集可能不能收敛。 在这里使用StandardScaler将数据集的特征进行缩放。详细信息请看StandardScaler。appendBias方法很简单，就是在每个向量后面加一个值为1的项。
 
 def appendBias(vector: Vector): Vector = {
     vector match {
@@ -126,10 +121,9 @@ def appendBias(vector: Vector): Vector = {
       case _ => throw new IllegalArgumentException(s"Do not support vector type ${vector.getClass}")
     }
 
-3.1.2 使用最优化算法计算最终的权重值
-
+//1.2、使用最优化算法计算最终的权重值
 val weightsWithIntercept = optimizer.optimize(data, initialWeightsWithIntercept)
-  有梯度下降算法和L-BFGS两种算法来计算最终的权重值，查看梯度下降法和L-BFGS了解详细实现。 这两种算法均使用Gradient的实现类计算梯度，使用Updater的实现类更新参数。在LogisticRegressionWithSGD和LogisticRegressionWithLBFGS中，它们均使用LogisticGradient实现类计算梯度，使用SquaredL2Updater实现类更新参数。
+//梯度下降算法和L-BFGS两种算法来计算最终的权重值，查看梯度下降法和L-BFGS了解详细实现。 这两种算法均使用Gradient的实现类计算梯度，使用Updater的实现类更新参数。在LogisticRegressionWithSGD和LogisticRegressionWithLBFGS中，它们均使用LogisticGradient实现类计算梯度，使用SquaredL2Updater实现类更新参数。
 
 //在GradientDescent中
 private val gradient = new LogisticGradient()
@@ -141,11 +135,10 @@ override val optimizer = new GradientDescent(gradient, updater)
     .setMiniBatchFraction(miniBatchFraction)
 //在LBFGS中
 override val optimizer = new LBFGS(new LogisticGradient, new SquaredL2Updater)
-  下面将详细介绍LogisticGradient的实现和SquaredL2Updater的实现。
 
-LogisticGradient
-  LogisticGradient中使用compute方法计算梯度。计算分为两种情况，即二元逻辑回归的情况和多元逻辑回归的情况。虽然多元逻辑回归也可以实现二元分类，但是为了效率，compute方法仍然实现了一个二元逻辑回归的版本。
-
+//下面将详细介绍LogisticGradient的实现和SquaredL2Updater的实现。
+//1.2.1、LogisticGradient
+//LogisticGradient中使用compute方法计算梯度。计算分为两种情况，即二元逻辑回归的情况和多元逻辑回归的情况。虽然多元逻辑回归也可以实现二元分类，但是为了效率，compute方法仍然实现了一个二元逻辑回归的版本。
 val margin = -1.0 * dot(data, weights)
 val multiplier = (1.0 / (1.0 + math.exp(margin))) - label
 //y += a * x，即cumGradient += multiplier * data
@@ -156,7 +149,7 @@ if (label > 0) {
 } else {
     MLUtils.log1pExp(margin) - margin
 }
-  这里的multiplier就是上文的公式(2)。axpy方法用于计算梯度，这里表示的意思是h(x) * x。下面是多元逻辑回归的实现方法。
+//这里的multiplier就是上文的公式(2)。axpy方法用于计算梯度，这里表示的意思是h(x) * x。下面是多元逻辑回归的实现方法。
 
 //权重
 val weightsArray = weights match {
@@ -221,7 +214,8 @@ if (maxMargin > 0) {
 } else {
      loss
 }
-SquaredL2Updater
+
+//1.2.2、SquaredL2Updater
 class SquaredL2Updater extends Updater {
   override def compute(
       weightsOld: Vector,
@@ -243,13 +237,12 @@ class SquaredL2Updater extends Updater {
     (Vectors.fromBreeze(brzWeights), 0.5 * regParam * norm * norm)
   }
 }
-  该函数的实现规则是：
+//该函数的实现规则是：
+//w' = w - thisIterStepSize * (gradient + regParam * w)
+//w' = (1 - thisIterStepSize * regParam) * w - thisIterStepSize * gradient
+//这里thisIterStepSize表示参数沿负梯度方向改变的速率，它随着迭代次数的增多而减小。
 
-w' = w - thisIterStepSize * (gradient + regParam * w)
-w' = (1 - thisIterStepSize * regParam) * w - thisIterStepSize * gradient
-  这里thisIterStepSize表示参数沿负梯度方向改变的速率，它随着迭代次数的增多而减小。
-
-3.1.3 对最终的权重值进行后处理
+//1.3、对最终的权重值进行后处理
 
 val intercept = if (addIntercept && numOfLinearPredictor == 1) {
       weightsWithIntercept(weightsWithIntercept.size - 1)
@@ -261,7 +254,7 @@ var weights = if (addIntercept && numOfLinearPredictor == 1) {
     } else {
       weightsWithIntercept
     }
-  该段代码获得了intercept以及最终的权重值。由于intercept和权重是在收缩的空间进行训练的，所以我们需要再把它们转换到元素的空间。数学显示，如果我们仅仅执行标准化而没有减去均值，即withStd = true, withMean = false， 那么intercept的值并不会发送改变。所以下面的代码仅仅处理权重向量。
+//该段代码获得了intercept以及最终的权重值。由于intercept和权重是在收缩的空间进行训练的，所以我们需要再把它们转换到元素的空间。数学显示，如果我们仅仅执行标准化而没有减去均值，即withStd = true, withMean = false， 那么intercept的值并不会发送改变。所以下面的代码仅仅处理权重向量。
 
 if (useFeatureScaling) {
       if (numOfLinearPredictor == 1) {
@@ -282,22 +275,21 @@ if (useFeatureScaling) {
         weights = Vectors.dense(weightsArray)
       }
     }
-3.1.4 创建模型
+//1.4、创建模型
 createModel(weights, intercept)
 
-3.2 预测
-训练完模型之后，我们就可以通过训练的模型计算得到测试数据的分类信息。predictPoint用来预测分类信息。它针对二分类和多分类，分别进行处理。
-
-二分类的情况
+//2、预测
+//训练完模型之后，我们就可以通过训练的模型计算得到测试数据的分类信息。predictPoint用来预测分类信息。它针对二分类和多分类，分别进行处理。
+//2.1、二分类的情况
 val margin = dot(weightMatrix, dataMatrix) + intercept
 val score = 1.0 / (1.0 + math.exp(-margin))
 threshold match {
     case Some(t) => if (score > t) 1.0 else 0.0
     case None => score
 }
-  我们可以看到1.0 / (1.0 + math.exp(-margin))就是上文提到的逻辑函数即sigmoid函数。
+//我们可以看到1.0 / (1.0 + math.exp(-margin))就是上文提到的逻辑函数即sigmoid函数。
 
-多分类情况
+//2.2、多分类情况
 var bestClass = 0
 var maxMargin = 0.0
 val withBias = dataMatrix.size + 1 == dataWithBiasSize
@@ -316,5 +308,5 @@ val withBias = dataMatrix.size + 1 == dataWithBiasSize
         }
 }
 bestClass.toDouble
-  该段代码计算并找到最大的margin。如果maxMargin为负，那么第一类是该数据的类别。
-  
+//该段代码计算并找到最大的margin。如果maxMargin为负，那么第一类是该数据的类别。
+
